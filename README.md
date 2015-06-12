cb8a53c09f4a893a582bcdea6db622b307b90358
Kalen Jordan <kalen@sparcksoft.com>
2015-06-12 11:36:49 -0700

    Got most of the config and javascript in place

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Better Visitor Log
 ==================
 
+Better visitor logging for your Magento store.
+
 ### Installation
 
 Go to https://magemail.co/better-visitor-log/
---
+++ app/code/community/KJ/BetterVisitorLog/Helper/Data.php
@@ -0,0 +1,33 @@
+<?php
+
+class KJ_BetterVisitorLog_Helper_Data extends Mage_Core_Helper_Data
+{
+    public function isLocalLoggingEnabled()
+    {
+        return Mage::getStoreConfig('system/kj_bettervisitorlog/enable_local_logging', Mage::app()->getStore());
+    }
+
+    public function isMixpanelLoggingEnabled()
+    {
+        return Mage::getStoreConfig('system/kj_bettervisitorlog/enable_mixpanel', Mage::app()->getStore());
+    }
+
+    public function getMixpanelToken()
+    {
+        return Mage::getStoreConfig('system/kj_bettervisitorlog/mixpanel_token', Mage::app()->getStore());
+    }
+
+    public function getLoggedInCustomerEmail()
+    {
+        if (! Mage::getSingleton('customer/session')->isLoggedIn()) {
+            return null;
+        }
+
+        $customer = Mage::getSingleton('customer/session')->getCustomer();
+        if (! $customer || ! ($customer instanceof Mage_Customer_Model_Customer)) {
+            return null;
+        }
+
+        return $customer->getData('email');
+    }
+}

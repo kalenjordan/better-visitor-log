a2cec3e1251f7624c6d03756c1ad7af16c33e08e
Kalen Jordan <kalen@sparcksoft.com>
2015-06-15 11:13:41 -0700

    Ajax request to log page view and capture email when typed

--- app/code/community/KJ/BetterVisitorLog/Helper/Data.php
+++ app/code/community/KJ/BetterVisitorLog/Helper/Data.php
@@ -2,6 +2,24 @@
 
 class KJ_BetterVisitorLog_Helper_Data extends Mage_Core_Helper_Data
 {
+    public function getAjaxBaseUrl()
+    {
+        $base = Mage::getBaseUrl(Mage_Core_Model_Store::URL_TYPE_LINK, $this->_getRequest()->isSecure());
+        $base .= "kjbvl";
+
+        return $base;
+    }
+
+    public function currentProductId()
+    {
+        $product = Mage::registry('current_product');
+        if (!($product instanceof Mage_Catalog_Model_Product)) {
+            return null;
+        }
+
+        return $product->getId();
+    }
+
     public function isLocalLoggingEnabled()
     {
         return Mage::getStoreConfig('system/kj_bettervisitorlog/enable_local_logging', Mage::app()->getStore());

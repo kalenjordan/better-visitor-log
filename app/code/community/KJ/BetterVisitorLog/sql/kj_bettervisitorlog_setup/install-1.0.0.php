b1d40e8bbb411d9a8b3f5336c6ef8d0cca0d5791
Kalen Jordan <kalen@sparcksoft.com>
2015-06-15 11:24:02 -0700

    Add indexes to some of the log fields and do some better ajax error handling

--- app/code/community/KJ/BetterVisitorLog/sql/kj_bettervisitorlog_setup/install-1.0.0.php
+++ app/code/community/KJ/BetterVisitorLog/sql/kj_bettervisitorlog_setup/install-1.0.0.php
@@ -23,7 +23,7 @@ $table->addColumn(
 
 $table->addColumn(
     'product_id',
-    Varien_Db_Ddl_Table::TYPE_VARCHAR,
+    Varien_Db_Ddl_Table::TYPE_INTEGER,
     null
 );
 
@@ -50,6 +50,9 @@ $table->addColumn(
 );
 
 try {
+    $table->addIndex('IDX_KJ_BETTERVISITORLOG_EMAIL', array('email'));
+    $table->addIndex('IDX_KJ_BETTERVISITORLOG_PRODUCT_ID', array('product_id'));
+    $table->addIndex('IDX_KJ_BETTERVISITORLOG_CREATED_AT', array('created_at'));
     $this->getConnection()->createTable($table);
 } catch (Exception $e) {
     Mage::logException($e);

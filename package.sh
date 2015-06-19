09e22e02dac5cef28777d8486a42bcdc556849ef
Kalen Jordan <kalen@sparcksoft.com>
2015-06-19 08:49:52 -0700

    Use the Connect packager

--- package.sh
+++ package.sh
@@ -12,12 +12,22 @@ echo "$COMMIT" >> ~/Desktop/BetterVisitorLog/app/code/community/KJ/BetterVisitor
 cd ~/Desktop/BetterVisitorLog/
 zip -r ../BetterVisitorLog.zip .
 
-rm -rf ~/Desktop/BetterVisitorLog/
+# Package up the magento connect package
+tar -cvf ../KJ_BetterVisitorLog.tar *
+cd ~/bin/magento-tar-to-connect
+php magento-tar-to-connect.php bettervisitorlog-config.php
+
+rm -rf ~/Desktop/BetterVisitorLog/ ~/Desktop/KJ_BetterVisitorLog.tar
 
 cd $CWD
 mv ~/Desktop/BetterVisitorLog.zip .
+mv ~/Desktop/build-connect/KJ_BetterVisitorLog*.tgz ./KJ_BetterVisitorLog.tgz
 
 scp BetterVisitorLog.zip serverpilot@magemail.co:~/apps/magemail/public/app/media/uploads/
-rm BetterVisitorLog.zip
+scp KJ_BetterVisitorLog.tgz serverpilot@magemail.co:~/apps/magemail/public/app/media/uploads
+
+rm BetterVisitorLog.zip KJ_BetterVisitorLog.tgz
+rm -rf ~/Desktop/build-connect
 
 echo "Deployed to https://magemail.co/BetterVisitorLog.zip"
+echo "Deployed to https://magemail.co/KJ_BetterVisitorLog.tgz"
